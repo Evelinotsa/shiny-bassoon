2db5370a44669d0aaaf62e58bd29107f9413924b
Evelinotsa <135664496+Evelinotsa@users.noreply.github.com>
2024-09-16 19:18:40 +0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import './App.css';
 import MyButton from "./components/MyButton";
 import MyContainer from './layout/MyContainer';
 import Login from './components/Login';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-  <MyContainer>
-    <Login />
-  </MyContainer>
+  //<MyContainer>
+   // <Login />
+  //</MyContainer>
+  <>
+    <h1>Header</h1>
+    <Outlet />
+  </>
   )
 }
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,13 +2,21 @@ import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { FormEvent, useState, useEffect } from "react";
 
 export default function MyForm() {
-  // const [form, setForm] = useState({ username: "", password: "" });
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [form, setForm] = useState({ username: "", password: "" });
+  //const [username, setUsername] = useState("");
+  //const [password, setPassword] = useState("");
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    // event.preventDefault();
-    // console.log(form);
+    event.preventDefault();
+    console.log(form);
+
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((prevForm) => ({
+        ...prevForm,
+        [event.target.name]: event.target.value,
+    }));
   };
   return (
     <Box
@@ -31,16 +39,16 @@ export default function MyForm() {
             name="username"
             label="Username"
             variant="outlined"
-            onChange={(event) => setUsername(event.target.value)}
-            // onChange={handleChange}
+            //onChange={(event) => setUsername(event.target.value)}
+            onChange={handleChange}
             // value={form.username}
           />
           <TextField
             name="password"
             label="Password"
             variant="outlined"
-            onChange={(event) => setPassword(event.target.value)}
-            // onChange={handleChange}
+            //onChange={(event) => setPassword(event.target.value)}
+            onChange={handleChange}
             // value={form.password}
           />
           <Button variant="contained" type="submit">
