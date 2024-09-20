ec476f37296b77e4631459baf1485612888decc8
Evelinotsa <135664496+Evelinotsa@users.noreply.github.com>
2024-09-20 18:39:25 +0300

    feat: add Prettier configuration file closes #1

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,23 +1,22 @@
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
-import { FormEvent, useState, useEffect } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material"
+import { FormEvent, useState, useEffect } from "react"
 
 export default function MyForm() {
-  const [form, setForm] = useState({ username: "", password: "" });
+  const [form, setForm] = useState({ username: "", password: "" })
   //const [username, setUsername] = useState("");
   //const [password, setPassword] = useState("");
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(form);
-
-  };
+    event.preventDefault()
+    console.log(form)
+  }
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setForm((prevForm) => ({
-        ...prevForm,
-        [event.target.name]: event.target.value,
-    }));
-  };
+    setForm(prevForm => ({
+      ...prevForm,
+      [event.target.name]: event.target.value,
+    }))
+  }
   return (
     <Box
       sx={{
@@ -51,11 +50,14 @@ export default function MyForm() {
             onChange={handleChange}
             // value={form.password}
           />
-          <Button variant="contained" type="submit">
+          <Button
+            variant="contained"
+            type="submit"
+          >
             Login
           </Button>
         </Stack>
       </form>
     </Box>
-  );
+  )
 }
