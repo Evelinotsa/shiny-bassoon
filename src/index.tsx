2e6fd0470e175e442466da265bc998ceae73613f
Evelinotsa <135664496+Evelinotsa@users.noreply.github.com>
2024-09-22 21:14:03 +0300

    feat: createHashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { createHashRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import MyForm from './components/Login';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -12,21 +12,20 @@ import Contact from './components/Contact';
 
 const hobbiesData = ["Cats", "Reading", "Sport", "Music"];
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route path='/home' element={<Home/>}/>
-      <Route path="/form" element={<MyForm />} />
-      <Route 
-        path="/hobbies" 
-        element={<Hobbies name="Evelin" hobbies={hobbiesData} />} 
-      />
-      <Route path='/login' element={<Login/>}/>
-      <Route path="/extra" element={<h1>Extra</h1>} />
-      <Route path="/contact" element={<Contact/>} />
-    </Route>
-  )
-)
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "home", element: <Home /> },
+      { path: "form", element: <MyForm /> },
+      { path: "hobbies", element: <Hobbies name="Evelin" hobbies={hobbiesData} /> },
+      { path: "login", element: <Login /> },
+      { path: "contact", element: <Contact /> },
+      { path: "extra", element: <h1>Extra</h1> }
+    ]
+  }
+]);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
